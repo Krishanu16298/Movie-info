2c35c9f0ac7fb2b47bdecb30c85c904dafc5fd20
Krishanu <mkrishanu98@gmail.com>
2018-09-07 19:42:55 +0530

    Speech added

--- main.js
+++ main.js
@@ -1,28 +1,78 @@
-let input = document.getElementById('movin');
-let input2 = document.getElementById('movie');
-input.addEventListener('submit',(e)=> {
-    console.log(input2.value);
-    let t = input2.value;
-    let output ='';
-    let title = '';
-    let year = '';
-    let img = '';
-    let id = '';
-    if(t !== null){
-        fetch('http://www.omdbapi.com/?s='+t+'&apikey=f9b9ce80')
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data.Search.length);
-            for(let i = 0; i < data.Search.length; i++){
-                console.log(data.Search[i].Title);
-                title = data.Search[i].Title;
-                year = data.Search[i].Year;
-                img = data.Search[i].Poster;
-                id = data.Search[i].imdbID;
-                if(img === 'N/A'){
-                    img = '1.jpeg';
-                }
-                output += `
+let input = document.getElementById("movin");
+let input2 = document.getElementById("movie");
+let speech = new webkitSpeechRecognition();
+
+let speechStart = () => {
+  speech.start();
+  document.getElementById("mic").innerHTML =
+    '<i class="fas fa-dot-circle fa-sm"></i>';
+};
+
+speech.onresult = e => {
+  t = e.results[0][0].transcript;
+  document.getElementById("mic").innerHTML =
+    '<i class="fas fa-microphone"></i>';
+  if (t !== null) {
+    fetch("http://www.omdbapi.com/?s=" + t + "&apikey=f9b9ce80")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        for (let i = 0; i < data.Search.length; i++) {
+          //   console.log(data.Search[i].Title);
+          title = data.Search[i].Title;
+          year = data.Search[i].Year;
+          img = data.Search[i].Poster;
+          id = data.Search[i].imdbID;
+          if (img === "N/A") {
+            img = "1.jpeg";
+          }
+          output += `
+                        <div class="col-md-3">
+                            <div class="card">
+                                <img src="${img}" class="card-img-top">
+                                <div class="card-body">
+                                    <div class="card-title">
+                                        <h3>${title}</h3>
+                                        <small>${year}</small>
+                                    </div>
+                                    <div class="card-text">
+                                        <button onclick="modinit('${id}')" data-toggle="modal" data-target=".bd-example-modal-lg" id="info" class="btn btn-outline-primary">Read more</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+          output = output.replace("[object HTMLDivElement]", "");
+        }
+        document.getElementById("output").innerHTML = output;
+        output = "";
+      });
+  }
+};
+
+input.addEventListener("submit", e => {
+  console.log(input2.value);
+  let t = input2.value;
+  let output = "";
+  let title = "";
+  let year = "";
+  let img = "";
+  let id = "";
+  if (t !== null) {
+    fetch("http://www.omdbapi.com/?s=" + t + "&apikey=f9b9ce80")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        for (let i = 0; i < data.Search.length; i++) {
+          //   console.log(data.Search[i].Title);
+          title = data.Search[i].Title;
+          year = data.Search[i].Year;
+          img = data.Search[i].Poster;
+          id = data.Search[i].imdbID;
+          if (img === "N/A") {
+            img = "1.jpeg";
+          }
+          output += `
                     <div class="col-md-3">
                         <div class="card">
                             <img src="${img}" class="card-img-top">
@@ -38,23 +88,24 @@ input.addEventListener('submit',(e)=> {
                         </div>
                     </div>
                 `;
-            }
-            document.getElementById('output').innerHTML = output;
-        });
-    }
-    e.preventDefault();
+        }
+        document.getElementById("output").innerHTML = output;
+        output = "";
+      });
+  }
+  e.preventDefault();
 });
 
-function modinit(str){
-    fetch('http://www.omdbapi.com/?i=' + str +'&apikey=f9b9ce80')
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        let img = data.Poster;
-        if (img === 'N/A') {
-            img = '1.jpeg';
-        }
-        document.getElementById('modbod').innerHTML = `
+function modinit(str) {
+  fetch("http://www.omdbapi.com/?i=" + str + "&apikey=f9b9ce80")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      let img = data.Poster;
+      if (img === "N/A") {
+        img = "1.jpeg";
+      }
+      document.getElementById("modbod").innerHTML = `
         <div class="row">
             <div class="col-6" id="img">
             <img src="${img}">
@@ -117,7 +168,6 @@ function modinit(str){
                     </div>
                 </div>
             </div>
-        </div>`
-        ;
+        </div>`;
     });
 }
